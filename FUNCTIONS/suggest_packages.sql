3c798fba2b4628aa1a4c520c28839960c3f599af
Joel Jakobsson <joel@compiler.org>
2021-05-05 02:29:16 +0200

    Fix bug failing to parse strace log due to not supporting multiple white spaces after PID
    Also, log the file_paths for each suggested package.

--- FUNCTIONS/suggest_packages.sql
+++ FUNCTIONS/suggest_packages.sql
@@ -17,7 +17,7 @@ INSERT INTO magicmake.strace
 SELECT
   log_line_text
 FROM regexp_split_to_table(pg_read_file(strace_log_file_path),E'\n') AS log_line_text
-WHERE log_line_text ~ '^(?:\d+ )?[a-z]+\(';
+WHERE log_line_text ~ '^(?:\d+ +)?[a-z]+\(';
 --
 -- match the strace rows against file_packages
 --
@@ -35,29 +35,25 @@ missing_files AS
 missing_packages AS
 (
   SELECT
-    packages
-  FROM missing_files
-  JOIN magicmake.file_packages
-    ON file_packages.file_name = missing_files.file_name
-  AND file_packages.file_path = ANY(missing_files.file_paths)
-),
-missing_package_names AS
-(
-  SELECT DISTINCT
     regexp_replace
     (
       --
       -- ignore multiple packages separated by comma,
       -- just pick the first one
       --
-      regexp_replace(packages,',.*$',''),
+      regexp_replace(file_packages.packages,',.*$',''),
       --
       -- extract the package name
       --
       '^.*?([^/]+)$',
       '\1'
-    ) AS package
-  FROM missing_packages
+    ) AS package,
+    array_agg(file_packages.file_path) AS file_paths
+  FROM missing_files
+  JOIN magicmake.file_packages
+    ON file_packages.file_name = missing_files.file_name
+  AND file_packages.file_path = ANY(missing_files.file_paths)
+  GROUP BY 1
 ),
 new_missing_packages AS
 (
@@ -67,14 +63,14 @@ new_missing_packages AS
   -- when prompted if a package should be installed
   --
   INSERT INTO magicmake.suggested_packages
-    (package)
+    (package, file_paths)
   SELECT
-    package
-  FROM missing_package_names
+    package, file_paths
+  FROM missing_packages
   WHERE NOT EXISTS
   (
     SELECT 1 FROM magicmake.suggested_packages
-    WHERE suggested_packages.package = missing_package_names.package
+    WHERE suggested_packages.package = missing_packages.package
   )
   RETURNING package
 )
--- TABLES/suggested_packages.sql
+++ TABLES/suggested_packages.sql
@@ -1,4 +1,6 @@
 CREATE TABLE magicmake.suggested_packages (
 package text NOT NULL,
+file_paths text[] NOT NULL,
+suggested_at timestamptz NOT NULL DEFAULT now(),
 PRIMARY KEY (package)
 );
