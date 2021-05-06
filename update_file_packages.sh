6b991f01be43227543568b0a04e0fe37b744a2d2
Joel Jakobsson <joel@compiler.org>
2021-05-06 14:06:57 +0200

    Preserve multiple blank spaces exactly in file names
    There was a thinko in the awk command, causing multiple white spaces
    to be replaced with a single white space.

--- update_file_packages.sh
+++ update_file_packages.sh
@@ -9,7 +9,7 @@ for apt_list in /var/lib/apt/lists/*.lz4
 do
   echo "\\\echo Importing $apt_list..."
   echo "COPY magicmake.file_packages (file_path, packages) FROM stdin;"
-  lz4 -c $apt_list | awk -F "[ \t]+" '{col=$NF;NF--;print $0 "\t" col}'
+  lz4 -c $apt_list | awk -F "[ \t]" '{col=$NF;NF--;while(length($NF)==0){NF--};print $0 "\t" col}'
   echo "\."
 done
 echo "CREATE INDEX file_packages_file_name ON magicmake.file_packages (file_name);"
